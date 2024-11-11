53f901a4e87edcfc73615d8b7b2e836f8888863c
Joshua Achermann <joshua.achermann@gmail.com>
2024-11-11 21:51:27 +1100

    Add missing err init

--- mumble_ptt.c
+++ mumble_ptt.c
@@ -58,6 +58,7 @@ static int init_libinput(void) {
 
 int dbus_send(char *command) {
   DBusError err;
+  dbus_error_init(&err);
   DBusMessage *msg = dbus_message_new_method_call("net.sourceforge.mumble.mumble", "/", "net.sourceforge.mumble.Mumble", command);
   if (!dbus_connection_send_with_reply_and_block(connection, msg, DBUS_TIMEOUT_USE_DEFAULT, &err)) {
     return 5;
